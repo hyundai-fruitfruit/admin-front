30a031bd708b6ccdd2624ccf4a254452e6776fc7
Choi-Seong-Hyeok <cinemay3@daum.net>
2024-02-21 13:56:26 +0900

    feat : 이벤트 등록 페이지

--- src/jsx/components/Forms/CkEditor/CkEditorBlog.jsx
+++ src/jsx/components/Forms/CkEditor/CkEditorBlog.jsx
@@ -1,3 +1,10 @@
+/**
+ * @author 최성혁
+ * @email [cinemay33@gmail.com]
+ * @create date 2024-02-21 13:33:39
+ * @modify date 2024-02-21 13:33:39
+ * @desc [description]
+ */
 import React from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
@@ -5,19 +12,17 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 
 
-const CkEditorBlog = () => {
+const CkEditorBlog = ({ onChange }) => {
     return (
-        <>
-            <CKEditor
-                editor={ ClassicEditor }           
-                
-                onChange={ ( event ) => {
-                    // const data = editor.getData();
-                    
-                } }               
-            />  
-        </>
+        <CKEditor
+            editor={ ClassicEditor }           
+            onChange={ (event, editor) => {
+                const data = editor.getData();
+                onChange(data); // props로 받은 onChange 핸들러를 호출
+            }}               
+        />  
     );
 };
+
 
 export default CkEditorBlog;
