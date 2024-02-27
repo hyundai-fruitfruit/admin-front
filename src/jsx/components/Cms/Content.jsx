ca036bfda57af30ae9c88d130c7e05b9534e5bce
osy <osy9757@gmail.com>
2024-02-27 12:05:43 +0900

    feat: qrcode 인식 모달 페이지구현

--- src/jsx/components/Cms/Content.jsx
+++ src/jsx/components/Cms/Content.jsx
@@ -6,120 +6,109 @@
  * @desc 이벤트 조회,삭제 기능
  */
 
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
-import Collapse from 'react-bootstrap/Collapse';
-import { useEvents } from '../../../hooks/useEvents.jsx'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Collapse from "react-bootstrap/Collapse";
+import { useEvents } from "../../../hooks/useEvents.jsx";
 
-const Content = () =>{
-    const { events,deleteEvent } = useEvents();
-    const [open2, setOpen2] = useState(true);
-    
-    // 이벤트 삭제 핸들러
-    const handleDelete = async (id) => {
-        if (window.confirm('이 이벤트를 삭제하시겠습니까?')) { 
-            await deleteEvent(id); 
-        }
-    };
+import EventModal from "../Modal/eventmodal/EventModal.jsx";
 
+const Content = () => {
+  const { events, deleteEvent } = useEvents();
+  const [open2, setOpen2] = useState(true);
+  const [modalShow, setModalShow] = useState(false);
+  const [selectedEventId, setSelectedEventId] = useState(null);
 
-    return(
-        <>            
-            {/* <PageTitle  activeMenu={'Content'} motherMenu={"CMS"} /> */}
-            <div className="row">
-                <div className="col-xl-12">
-                    <div className="filter cm-content-box box-primary">
-                    </div>
-                    <div className="mb-3">
-                        <Link to={"/add-content"} className="btn btn-primary btn-sm">이벤트 등록</Link>
-                    </div>
-                    <div className="filter cm-content-box box-primary">
-                        <div className={`content-title`}
-                            onClick={() => setOpen2(!open2)}
-                        >
-                            <div className="cpa">
-                                <i className="fa-solid fa-file-lines me-2" />이벤트 리스트
-                            </div>
-                            <div className="tools">
-                                <Link to={"#"} className={`SlideToolHeader ${open2 ? 'collapse' : 'expand' }`}
-                                >
-                                    <i className="fas fa-angle-up"></i>
-                                </Link>
-                            </div>
-                        </div>
-                        <Collapse in={open2}>
-                            <div className="cm-content-body form excerpt">
-                                <div className="card-body py-3">
-                                    <div className="table-responsive order-list-table">
-                                        <div id="content_wrapper" className="dataTables_wrapper no-footer">
-                                            <table className="table table-responsive-lg table-condensed flip-content">
-                                                <thead>
-                                                    <tr>
-                                                        <th className='text-black'>ID</th>
-                                                        <th className='text-black'>제목</th>
-                                                        <th className='text-black'>시작일</th>
-                                                        <th className='text-black'>종료일</th>
-                                                        <th className='text-black'>시작시간</th>
-                                                        <th className='text-black'>마감시간</th>
-                                                        <th className='text-black'>최대 인원수</th>
-                                                        <th className='text-black'>현재 참여 인원수</th>
-                                                        <th className="text-black text-end">Actions</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    {events.map((item, ind) => (
-                                                        <tr key={ind}>
-                                                            <td>{item.id}</td>
-                                                            <td>{item.title}</td>
-                                                            <td>{item.startedAt}</td>
-                                                            <td>{item.finishedAt}</td>
-                                                            <td>
-                                                                {item.eventActiveTimeZoneDto?.length > 0 ? (
-                                                                    item.eventActiveTimeZoneDto.map((timeZone, index) => (
-                                                                        <div key={index}>
-                                                                        {timeZone.opennedAt} : 00
-                                                                        </div>
-                                                                    ))
-                                                                ) : (
-                                                                    '기본값'
-                                                                )}
-                                                            </td>
-                                                            <td>
-                                                                {item.eventActiveTimeZoneDto?.length > 0 ? (
-                                                                    item.eventActiveTimeZoneDto.map((timeZone, index) => (
-                                                                        <div key={index}>
-                                                                            {timeZone.closedAt} : 00
-                                                                        </div>
-                                                                    ))
-                                                                ) : (
-                                                                    '기본값'
-                                                                )}
-                                                            </td>
-                                                            <td>{item.maxCount}</td>
-                                                            <td>{item.visitedCount}</td>
-                                                            <td className='text-end'>
-                                                                <Link to={`/add-content/${item.id}`} className="btn btn-warning btn-sm content-icon me-1">
-                                                                    <i className="fa fa-edit"></i>
-                                                                </Link>
-                                                                <Link to={"#"} className="btn btn-danger btn-sm content-icon ms-1"
-                                                                    onClick={()=>handleDelete(item.id)}
-                                                                >
-                                                                    <i className="fa fa-times"></i>
-                                                                </Link>
-                                                            </td>
-                                                        </tr>
-                                                    ))}
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </Collapse> 
-                    </div>
-                </div>
+  // 이벤트 삭제 핸들러
+  const handleDelete = async (id) => {
+    if (window.confirm("이 이벤트를 삭제하시겠습니까?")) {
+      await deleteEvent(id);
+    }
+  };
+
+  const handleModalOpen = (id) => {
+    setSelectedEventId(id);
+    setModalShow(true);
+  };
+
+  return (
+    <>
+      {/* <PageTitle  activeMenu={'Content'} motherMenu={"CMS"} /> */}
+      <div className="row">
+        <div className="col-xl-12">
+          <div className="filter cm-content-box box-primary"></div>
+          <div className="mb-3">
+            <Link to={"/add-content"} className="btn btn-primary btn-sm">
+              이벤트 등록
+            </Link>
+          </div>
+          <div className="filter cm-content-box box-primary">
+            <div className={`content-title`} onClick={() => setOpen2(!open2)}>
+              <div className="cpa">
+                <i className="fa-solid fa-file-lines me-2" />
+                이벤트 리스트
+              </div>
+              <div className="tools">
+                <Link to={"#"} className={`SlideToolHeader ${open2 ? "collapse" : "expand"}`}>
+                  <i className="fas fa-angle-up"></i>
+                </Link>
+              </div>
             </div>
-        </>
-    )
-}
+            <Collapse in={open2}>
+              <div className="cm-content-body form excerpt">
+                <div className="card-body py-3">
+                  <div className="table-responsive order-list-table">
+                    <div id="content_wrapper" className="dataTables_wrapper no-footer">
+                      <table className="table table-responsive-lg table-condensed flip-content">
+                        <thead>
+                          <tr className="text-center">
+                            <th className="text-black">ID</th>
+                            <th className="text-black">제목</th>
+                            <th className="text-black">시작일</th>
+                            <th className="text-black">종료일</th>
+                            <th className="text-black">시작시간</th>
+                            <th className="text-black">마감시간</th>
+                            <th className="text-black">최대 인원수</th>
+                            <th className="text-black">현재 참여 인원수</th>
+                            <th className="text-black">Actions</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {events.map((item, ind) => (
+                            <tr key={ind} className="text-center">
+                              <td>{item.id}</td>
+                              <td>{item.title}</td>
+                              <td>{item.startedAt}</td>
+                              <td>{item.finishedAt}</td>
+                              <td>{item.eventActiveTimeZoneDto?.length > 0 ? item.eventActiveTimeZoneDto.map((timeZone, index) => <div key={index}>{timeZone.opennedAt} : 00</div>) : "기본값"}</td>
+                              <td>{item.eventActiveTimeZoneDto?.length > 0 ? item.eventActiveTimeZoneDto.map((timeZone, index) => <div key={index}>{timeZone.closedAt} : 00</div>) : "기본값"}</td>
+                              <td>{item.maxCount}</td>
+                              <td>{item.visitedCount}</td>
+                              <td className="">
+                                <Link to="#" className="btn btn-success btn-sm content-icon me-1" onClick={() => handleModalOpen(item.id)}>
+                                  <i className="fa fa-qrcode"></i>
+                                </Link>
+                                <Link to={`/add-content/${item.id}`} className="btn btn-warning btn-sm content-icon me-1">
+                                  <i className="fa fa-edit"></i>
+                                </Link>
+                                <Link to={"#"} className="btn btn-danger btn-sm content-icon me-1" onClick={() => handleDelete(item.id)}>
+                                  <i className="fa fa-times"></i>
+                                </Link>
+                              </td>
+                            </tr>
+                          ))}
+                          <EventModal show={modalShow} handleClose={() => setModalShow(false)} eventId={selectedEventId} />
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Collapse>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default Content;
---
+++ src/jsx/components/Modal/eventmodal/EventModal.jsx
@@ -0,0 +1,47 @@
+/**
+ * @author 오수영
+ * @email osy9757@gmail.com
+ * @create date 2024-02-27 11:34:12
+ * @modify date 2024-02-27 11:34:12
+ * @desc 이벤트 QR코드 인식 모달
+ */
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+
+import QRScannerComponent from "jsx/components/QrScanner/QRScannerComponent";
+
+const EventModal = ({ show, handleClose, eventId }) => {
+  const [scanResult, setScanResult] = useState("");
+
+  const handleScanData = (data) => {
+    if (data) {
+      setScanResult(data);
+    }
+  };
+
+  return (
+    <Modal show={show} onHide={handleClose} size="lg">
+      <Modal.Header closeButton>
+        <Modal.Title>이벤트 QR 코드 스캔</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="w-full h-full">
+          <QRScannerComponent handleScan={handleScanData} className="w-full h-full" />
+        </div>
+        {scanResult && (
+          <div>
+            <strong>스캔 결과:</strong> {scanResult}
+          </div>
+        )}
+        {!scanResult && <div>QR 코드를 스캔하여 이벤트 정보를 확인하세요. 이벤트 ID: {eventId}</div>}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          닫기
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default EventModal;
