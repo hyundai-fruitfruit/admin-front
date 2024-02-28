f82f6822e77515738d0a4d46158da8d084d9edde
tori <sangeun99@khu.ac.kr>
2024-02-28 15:19:00 +0900

    feat: 어드민 이벤트 참여 완료 시 소켓 발행

--- src/hooks/useSocket.jsx
+++ src/hooks/useSocket.jsx
@@ -1,22 +1,29 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import SockJS from 'sockjs-client';
 import { Client } from '@stomp/stompjs';
 
+/**
+ * @author 엄상은
+ * @email sangeun.e.9@gmail.com
+ * @create date 2024-02-28 14:41:35
+ * @modify date 2024-02-28 14:41:35
+ * @desc 소켓 hook 분리
+ */
+
 export function useSocket() {
+    const [stompClient, setStompClient] = useState(null);
+
     useEffect(() => {
         const socket = new SockJS(`${process.env.REACT_APP_API_BASE_URL}/websocket`);
-        const stompClient = new Client({
+        const client = new Client({
             webSocketFactory: () => socket,
             onConnect: function (frame) {
                 console.log('Socket Connected: ' + frame);
-
-                // 소켓 발행
-                stompClient.publish({
-                    destination: '/app/hello',
-                    body: '어드민 클라이언트에서 보낸 메시지'
-                });
             }
         });
-        stompClient.activate();
+        client.activate();
+        setStompClient(client);
     }, []);
+
+    return stompClient;
 }
--- src/jsx/components/QrScanner/QRScannerComponent.jsx
+++ src/jsx/components/QrScanner/QRScannerComponent.jsx
@@ -10,7 +10,7 @@
  * @email sangeun.e.9@gmail.com
  * @create date 2024-02-28 10:41:35
  * @modify date 2024-02-28 10:41:35
- * @desc QR API 연결
+ * @desc QR API 연결 및 소켓 연결
  */
 import React, { useState, useEffect } from "react";
 import QrScanner from "react-qr-scanner";
@@ -22,15 +22,21 @@ const QRScannerComponent = ({ eventId, handleSuccess }) => {
   const [lastScannedTime, setLastScannedTime] = useState(0);
   const [memberId, setMemberId] = useState("");
   const { data, loading, error } = useParticipateEvent(eventId, memberId);
-
-  useSocket();
   
+  const stompClient = useSocket();
+
   useEffect(() => {
-    if (data) {
+    if (data?.success) {
       console.log("data", data.success);
-      handleSuccess(data.success); // data.success 값이 변경될 때마다 부모 컴포넌트에 알립니다.
+      handleSuccess(data.success);
+
+      // 소켓 발행
+      stompClient.publish({
+        destination: '/app/hello',
+        body: 'Hello WebSocket',
+      });
     }
-  }, [data]);
+  }, [data, stompClient]); 
 
   const handleScan = (data) => {
     const now = Date.now();
